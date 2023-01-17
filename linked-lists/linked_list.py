fcfd31b3ce6b9b08812ec968885fdb92fef6799f
Pedro Ramos <ramuspedro@gmail.com>
2023-01-17 09:07:34 -0300

    update: singly linked list

--- linked-lists/linked_list.py
+++ linked-lists/linked_list.py
@@ -83,10 +83,80 @@ class LinkedList(object):
                     current.next = newNode 
                     self.length += 1
 
-        # method to delete the first node of the linked list 
-        def deleteFromBeginning(self):
-            if self.length == 0:
-                print ("The list is empty") 
+    # method to delete the first node of the linked list 
+    def deleteFromBeginning(self):
+        if self.length == 0:
+            print ("The list is empty") 
+        else:
+            self.head = self.head.next 
+            self.length -= 1
+    
+    #Method to delete the last node of the linked list 
+    def deleteLastNodeFromSinglyLinkedList(self):
+        if self.length == 0:
+            print ("The list is empty")
+        else:
+            currentnode = self.head 
+            previousnode = self.head
+            while currentnode.next != None:
+                previousnode = currentnode 
+                currentnode = currentnode.next
+            previousnode.next = None
+            self.length -= 1
+    
+    #Delete with node from linked list
+    def deleteFromLinkedListWithNode(self, node):
+        if self.length == 0:
+            raise ValueError("List is empty")
+        else:
+            current = self.head 
+            previous = None 
+            found = False
+            while not found:
+                if current == node:
+                    found = True
+                elif current is None:
+                    raise ValueError("Node not in Linked List") 
+                else:
+                    previous = current
+                    current = current.next 
+            if previous is None:
+                self.head = current.next 
             else:
-                self.head = self.head.next 
+                previous = current.next
+            self.length -= 1
+            
+    #Delete with data from linked list 
+    def deleteWithValue(self,value):
+        currentnode = self.head 
+        previousnode = self.head
+        while currentnode.next != None or currentnode.data != value: 
+            if currentnode.data == value:
+                previousnode.next = currentnode.next 
                 self.length -= 1
+                return
+            else:
+                previousnode = currentnode 
+                currentnode = currentnode.next
+        print ("The value provided is not present")
+
+    #Method to delete a node at a particular position 
+    def deleteAtPosition(self,pos):
+        count = 0
+        currentnode = self.head 
+        previousnode = self.head
+        if pos > self.length or pos < 0:
+            print ("The position does not exist. Please enter a valid position")
+        else:
+            while currentnode.next != None or count < pos:
+                count = count + 1 
+                if count == pos:
+                    previousnode.next = currentnode.next 
+                    self.length -= 1
+                    return
+                else:
+                    previousnode = currentnode 
+                    currentnode = currentnode.next
+
+    def clear( self ): 
+        self.head = None
